1eaf8257f1dc1cf190059bcd0624b3f46ebe004c
Andreas Forsberg <andreas.bengt.forsberg@gmail.com>
2023-12-01 13:46:59 +0100

    updated python validation file

--- validate_python_path.py
+++ validate_python_path.py
@@ -7,21 +7,19 @@ import os
 import os.path
 
 src_path_os = os.getenv("PYTHONPATH")
-src_path_str = "c:/Users/anforsbe/Visual Studio Code/SCMBI/cicd_code_resease/src"
+src_path_str = "c:/Users/anforsbe/Visual Studio Code/SCMBI/cicd_code_resease"
 
-print(src_path_os)
-print(src_path_str)
+print(f"src_path_os: {src_path_os}")
+print(f"src_path_str: {src_path_str}")
 
-print(sys.path[0])
+for idx, (os_path_char, string_path_char) in enumerate(zip(src_path_os, src_path_str)):
 
-# for idx, (os_path_char, string_path_char) in enumerate(zip(src_path_os, src_path_str)):
+    if os_path_char == string_path_char:
+        print(f"Equal at idx: {idx}. The char is: {os_path_char}, {string_path_char}")
+    else:
+        print(f"NOT equel at idx: {idx}. The char is: {os_path_char}, {string_path_char}")
 
-#     if os_path_char == string_path_char:
-#         print(f"Equal at idx: {idx}. The char is: {os_path_char}, {string_path_char}")
-#     else:
-#         print(f"NOT EQUAL at idx: {idx}. The char is: {os_path_char}, {string_path_char}")
-
-# if os.path.exists(src_path_os):
-#     print("dir exists")
-# else:
-#     print("Does not exists")
+if os.path.exists(src_path_os):
+    print("Dir exists")
+else:
+    print("Dir does NOT exists")
